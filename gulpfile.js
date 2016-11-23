f2bc0ddf4984e8fb896ad0fb2b6050f2fdf9fa38
Hoang Minh Thang <hoangminhthang21051995@gmail.com>
2016-11-23 23:57:28 +0700

    installed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,21 @@ require('laravel-elixir-vue');
  |
  */
 
-elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+// elixir(mix => {
+//     mix.sass('app.scss')
+//        .webpack('app.js');
+// });
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('less', function () {
+    return gulp.src('./resources/assets/less/app.less').pipe(less({
+        paths: [path.join(__dirname, 'less', 'includes')]
+    })).pipe(gulp.dest('./resources/assets/css'));
 });
+
+gulp.task('default', ['less'], function () {
+    
+})
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,20 @@
 @extends('layout')
 
 @section('content')
+    <br>
+    <br>
+    <h1>CONTENT</h1>
+    <h1>CONTENT</h1>
+    <h1>CONTENT</h1>
+    <h1>CONTENT</h1>
+    <h1>CONTENT</h1>
+    <h1>CONTENT</h1>
+    <h1>CONTENT</h1>
+    <h1>CONTENT</h1>
+    <h1>CONTENT</h1>
+    <br>
+    <br>
+    <br>
+    <br>
+
 @stop
